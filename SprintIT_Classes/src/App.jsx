51637b15318d9b4973a9ac956011c34e6a2d3f2e
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2026-01-23 15:00:46 +0530

    first commit

---
+++ SprintIT_Classes/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-red-200">
+      
+    </div>
+  )
+}
+
+export default App
